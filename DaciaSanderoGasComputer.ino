e983fc71ebc86e48f2f8dd2bd7ce17fab2d9d27e
Maciej Czajkowski <czajoms@gmail.com>
2025-06-18 11:54:56 +0200

    dodanie plików logo i opracowanie layoutu wyświetlania danych
    jeszcze nie liczy spalania

--- DaciaSanderoGasComputer.ino
+++ DaciaSanderoGasComputer.ino
@@ -1,101 +1,226 @@
-#include "BluetoothSerial.h"
-#include "ELMduino.h"
 #include <Adafruit_GFX.h>
 #include <Adafruit_ST7789.h>
+#include <SPI.h>
+#include <Fonts/FreeSans9pt7b.h>
+#include "BluetoothSerial.h"
+#include "ELMduino.h"
+#include "layout.h"
+#include "logo.h"
+#include "sandero.h"
 
 // Piny ekranu
+#define LCD_MOSI 23
+#define LCD_SCLK 18
 #define LCD_CS 15
 #define LCD_DC 2
 #define LCD_RST 4
 #define LCD_BLK 32
 
-#define TFT_WIDTH 320
-#define TFT_HEIGHT 170
+// Szerokość i wysokość ekranu
+#define SCREEN_WIDTH 320
+#define SCREEN_HEIGHT 170
 
+// Wysokość górnej sekcji (30% wysokości ekranu)
+#define TOP_HEIGHT (SCREEN_HEIGHT * 30 / 100)
+// Szerokość lewej górnej sekcji (75% szerokości ekranu)
+int topLeftW = (SCREEN_WIDTH * 3) / 4;
+// Szerokość prawej górnej sekcji
+int topRightW = SCREEN_WIDTH - topLeftW;
+// Wysokość lewej górnej sekcji
+int topLeftH = TOP_HEIGHT;
+// Wysokość prawej górnej sekcji
+int topRightH = TOP_HEIGHT;
+
+// Obiekt TFT
 Adafruit_ST7789 tft = Adafruit_ST7789(LCD_CS, LCD_DC, LCD_RST);
 
+// Obiekty Bluetooth i ELM327
 BluetoothSerial SerialBT;
 #define ELM_PORT SerialBT
 #define DEBUG_PORT Serial
 
-uint8_t address[6] = { 0xDC, 0x0D, 0x30, 0x9F, 0x96, 0x0E };  // PRZYKŁADOWY ADRES! ZMIEŃ GO!
+// Pamiętaj, ABY ZMIENIĆ TEN ADRES MAC NA ADRES SWOJEGO URZĄDZENIA OBD-II!
+uint8_t elm327_address[6] = { 0xDC, 0x0D, 0x30, 0x9F, 0x96, 0x0E };
 
 ELM327 myELM327;
 
+// Zmienne do przechowywania odczytów z OBD
+uint32_t current_rpm = 0;
+uint8_t current_speed = 0;
+float current_fuel_consumption = 0.0;
+float current_oil_temp = 0.0;
+float current_coolant_temp = 0.0;
 
-uint32_t rpm = 0;
-uint8_t speed = 0;
 // Kolory
 #define COLOR_HEADER ST77XX_CYAN
 #define COLOR_VALUE ST77XX_WHITE
 #define COLOR_ERROR ST77XX_RED
 
-void showCenteredText(const String text, uint16_t color) {
-  tft.fillScreen(ST77XX_BLACK);
-  tft.setTextWrap(false);
-  tft.setTextSize(2);
-  tft.setTextColor(color);
+// --- Zmiany tutaj ---
+// Definicja stanów odczytu OBD
+enum OBD_STATE {
+  ENG_RPM,
+  SPEED,
+  OIL_TEMP,  // Dodajemy nowe stany dla kolejnych odczytów
+  COOLANT_TEMP,
+  FUEL_CONS  // Zużycie paliwa (jeśli będzie implementowane na podstawie PID)
+};
 
-  int16_t x1, y1;
-  uint16_t w, h;
-  tft.getTextBounds(text, 0, 0, &x1, &y1, &w, &h);
+// Zmienna stanu
+OBD_STATE obd_state = ENG_RPM;
 
-  int16_t x = (TFT_WIDTH - w) / 2 - x1;
-  int16_t y = (TFT_HEIGHT - h) / 2 - y1;
+// Funkcja, która będzie odczytywać i wyświetlać dane z OBD
+void readAndDisplayOBD() {
+  switch (obd_state) {
+    case ENG_RPM:
+      {
+        float tempRPM = myELM327.rpm();
+        if (myELM327.nb_rx_state == ELM_SUCCESS) {
+          current_rpm = (uint32_t)tempRPM;
+          DEBUG_PORT.print("RPM: ");
+          DEBUG_PORT.println(current_rpm);
+          obd_state = SPEED;  // Przejście do następnego stanu
+        } else if (myELM327.nb_rx_state != ELM_GETTING_MSG) {
+          myELM327.printError();
+          current_rpm = 0;    // Ustaw 0 w przypadku błędu
+          obd_state = SPEED;  // Przejście do następnego stanu nawet w przypadku błędu
+        }
+        break;
+      }
 
-  tft.setCursor(x, y);
-  tft.print(text);
+    case SPEED:
+      {
+        float tempSpeed = myELM327.kph();  // Zmieniamy na kph dla km/h
+        if (myELM327.nb_rx_state == ELM_SUCCESS) {
+          current_speed = (uint8_t)tempSpeed;
+          DEBUG_PORT.print("Speed: ");
+          DEBUG_PORT.print(current_speed);
+          DEBUG_PORT.println(" km/h");
+          obd_state = OIL_TEMP;  // Przejście do następnego stanu
+        } else if (myELM327.nb_rx_state != ELM_GETTING_MSG) {
+          myELM327.printError();
+          current_speed = 0;     // Ustaw 0 w przypadku błędu
+          obd_state = OIL_TEMP;  // Przejście do następnego stanu
+        }
+        break;
+      }
+
+    case OIL_TEMP:
+      {
+              {
+        float tempOil = myELM327.oilTemp();
+        if (myELM327.nb_rx_state == ELM_SUCCESS) {
+          current_oil_temp = tempOil;
+          DEBUG_PORT.print("Oil Temp: ");
+          DEBUG_PORT.print(current_oil_temp);
+          DEBUG_PORT.println(" C");
+          obd_state = COOLANT_TEMP;
+        } else if (myELM327.nb_rx_state != ELM_GETTING_MSG) {
+          myELM327.printError();
+          current_oil_temp = randFloat0to25() + 60;  // Losowa wartość w przypadku błędu
+          obd_state = COOLANT_TEMP;
+        }
+        break;
+      }
+      }
+      break;
+
+    case COOLANT_TEMP:
+      {
+        float tempCoolant = myELM327.engineCoolantTemp();
+        if (myELM327.nb_rx_state == ELM_SUCCESS) {
+          current_coolant_temp = tempCoolant;
+          DEBUG_PORT.print("Coolant Temp: ");
+          DEBUG_PORT.print(current_coolant_temp);
+          DEBUG_PORT.println(" C");
+          obd_state = FUEL_CONS;
+        } else if (myELM327.nb_rx_state != ELM_GETTING_MSG) {
+          myELM327.printError();
+          current_coolant_temp = randFloat0to25() + 60;  // Losowa wartość w przypadku błędu
+          obd_state = FUEL_CONS;
+        }
+        break;
+      }
+
+    case FUEL_CONS:
+      {
+        // Obliczenie spalania wymaga wielu PIDów (MAF, VSS, itp.) i jest złożone.
+        // Na potrzeby tego przykładu, użyjemy wartości losowej.
+        current_fuel_consumption = randFloat0to25();
+        DEBUG_PORT.print("Fuel Consumption: ");
+        DEBUG_PORT.print(current_fuel_consumption, 1);
+        DEBUG_PORT.println(" L/100km");
+        obd_state = ENG_RPM;  // Powrót do pierwszego stanu, aby cykl się powtarzał
+        break;
+      }
+  }
 }
 
+// Funkcje pomocnicze do generowania losowych danych
+float randFloat0to25() {
+  return ((float)rand() / (float)RAND_MAX) * 25.0;
+}
+
+int randInt(int min, int max) {
+  return min + rand() % (max - min + 1);
+}
 
 void setup() {
-#if LED_BUILTIN
-  pinMode(LED_BUILTIN, OUTPUT);
-  digitalWrite(LED_BUILTIN, LOW);
-#endif
-  // TFT init
-  tft.init(170, 320);  // szer x wys
+  DEBUG_PORT.begin(115200);
+
+  tft.init(170, 320);
   tft.setRotation(3);
   pinMode(LCD_BLK, OUTPUT);
   digitalWrite(LCD_BLK, HIGH);
+  tft.setFont(&FreeSans9pt7b);
 
-  DEBUG_PORT.begin(115200);
-  showCenteredText("Podlaczanie do auta", ST77XX_BLUE);
+  setDisplay(&tft);  // Ustawienie wskaźnika do obiektu TFT w display.h
+
+  tft.drawRGBBitmap(0, 0, logo, 320, 170);
+  delay(3000);
+  tft.drawRGBBitmap(0, 0, bootlogo, 320, 170);
+  delay(3000);
+
+  // --- Proces połączenia z OBD-II ---
+  showCenteredStatusText("Podlaczanie do auta", ST77XX_BLUE);
   SerialBT.setPin("1234", 4);
-  ELM_PORT.begin("Czajo_x5pro", true); //nazwa urządzenia poprzednio podpiętego pod iCara
+  ELM_PORT.begin("Czajo_x5pro", true);
 
-  if (!ELM_PORT.connect(address)) {
-    DEBUG_PORT.println("Couldn't connect to OBD scanner - Phase 1");
-    showCenteredText("Brak polaczenia z BT", ST77XX_RED);
+  DEBUG_PORT.println("Connecting to OBD scanner via Bluetooth...");
+  if (!ELM_PORT.connect(elm327_address)) {
+    DEBUG_PORT.println("Couldn't connect to OBD scanner - Phase 1 (Bluetooth)");
+    showCenteredStatusText("Brak polaczenia z BT", ST77XX_RED);
     while (1)
       ;
   }
+  DEBUG_PORT.println("Bluetooth connected.");
+  showCenteredStatusText("Polaczono z BT", ST77XX_YELLOW);
+  delay(1000);
 
+  DEBUG_PORT.println("Initializing ELM327...");
   if (!myELM327.begin(ELM_PORT, false, 2000)) {
-    Serial.println("Couldn't connect to OBD scanner - Phase 2");
-    showCenteredText("Brak polaczenia z OBD", ST77XX_RED);
+    DEBUG_PORT.println("Couldn't connect to OBD scanner - Phase 2 (ELM327)");
+    showCenteredStatusText("Brak polaczenia z OBD", ST77XX_RED);
     while (1)
       ;
   }
+  DEBUG_PORT.println("Connected to ELM327");
+  showCenteredStatusText("Polaczono z OBD", ST77XX_GREEN);
+  delay(1000);
 
-  Serial.println("Connected to ELM327");
-  showCenteredText("Polaczono z OBD", ST77XX_GREEN);
-}
-
-void wyswietlrpm() {
-float tempRPM = myELM327.rpm();
-
-
-  if (myELM327.nb_rx_state == ELM_SUCCESS) {
-    rpm = (uint32_t)tempRPM;
-    Serial.print("RPM: ");
-    Serial.println(rpm);
-    showCenteredText("RPM: " + (String)rpm, ST77XX_GREEN);
-  } else if (myELM327.nb_rx_state != ELM_GETTING_MSG)
-    myELM327.printError();
-  delay(100);
+  // Rysowanie początkowego układu na ekranie
+  drawLayout(SCREEN_WIDTH, SCREEN_HEIGHT, TOP_HEIGHT, topLeftW, topLeftH, topRightW, topRightH);
 }
 
 void loop() {
-  wyswietlrpm();
+  readAndDisplayOBD();  // Wywołujemy nową funkcję do odczytu danych
+
+  // RYSOWANIE DANYCH NA EKRANIE (teraz zaktualizowane zmienne globalne)
+  drawFuelConsumption(current_fuel_consumption, 0, 0, topLeftW, topLeftH);
+  drawOilTemp(current_oil_temp, 0, TOP_HEIGHT, SCREEN_WIDTH / 2, (SCREEN_HEIGHT - TOP_HEIGHT) / 2);
+  drawCoolantTemp(current_coolant_temp, SCREEN_WIDTH / 2, TOP_HEIGHT, SCREEN_WIDTH / 2, (SCREEN_HEIGHT - TOP_HEIGHT) / 2);
+  drawRPM(current_rpm, 0, TOP_HEIGHT + (SCREEN_HEIGHT - TOP_HEIGHT) / 2, SCREEN_WIDTH / 2, (SCREEN_HEIGHT - TOP_HEIGHT) / 2);
+  drawSpeed(current_speed, SCREEN_WIDTH / 2, TOP_HEIGHT + (SCREEN_HEIGHT - TOP_HEIGHT) / 2, SCREEN_WIDTH / 2, (SCREEN_HEIGHT - TOP_HEIGHT) / 2);
+
+  delay(100);  // Krótkie opóźnienie, aby nie obciążać CPU i pozwolić na stabilne odczyty
 }
---
+++ layout.h
@@ -0,0 +1,237 @@
+#ifndef DISPLAY_H
+#define DISPLAY_H
+
+#include <Adafruit_GFX.h>
+#include <Adafruit_ST7789.h>
+#include <SPI.h>
+
+// Deklaracja zewnętrznego obiektu TFT
+extern Adafruit_ST7789 *tftPtr; // Używamy wskaźnika do obiektu Adafruit_ST7789
+
+// Funkcja do ustawienia wskaźnika do obiektu TFT
+void setDisplay(Adafruit_ST7789 *display) {
+  tftPtr = display;
+}
+
+// Stałe dla rozmiarów ikony strzałki
+#define ARROW_W 8
+#define ARROW_H 9
+
+// Bitmapa dla strzałki (zielona)
+const uint16_t arrow_rgb565[64] = {
+  0x0000, 0x0000, 0x0000, 0x0000, 0x07E0, 0x07E0, 0x07E0, 0x0000,
+  0x0000, 0x0000, 0x07E0, 0x07E0, 0x07E0, 0x07E0, 0x0000, 0x0000,
+  0x0000, 0x07E0, 0x07E0, 0x07E0, 0x07E0, 0x07E0, 0x07E0, 0x0000,
+  0x07E0, 0x07E0, 0x07E0, 0x07E0, 0x07E0, 0x07E0, 0x07E0, 0x07E0,
+  0x0000, 0x07E0, 0x07E0, 0x07E0, 0x07E0, 0x07E0, 0x07E0, 0x0000,
+  0x0000, 0x0000, 0x07E0, 0x07E0, 0x07E0, 0x07E0, 0x0000, 0x0000,
+  0x0000, 0x0000, 0x0000, 0x07E0, 0x07E0, 0x0000, 0x0000, 0x0000,
+  0x0000, 0x0000, 0x0000, 0x0000, 0x07E0, 0x0000, 0x0000, 0x0000,
+};
+
+// Deklaracja wskaźnika do obiektu Adafruit_ST7789
+Adafruit_ST7789 *tftPtr;
+
+/**
+ * @brief Rysuje bitmapę strzałki na wyświetlaczu.
+ * @param x Współrzędna X lewego górnego rogu bitmapy.
+ * @param y Współrzędna Y lewego górnego rogu bitmapy.
+ */
+void drawExampleBitmap(int x, int y) {
+  if (tftPtr) {
+    tftPtr->drawRGBBitmap(x, y, arrow_rgb565, ARROW_W, ARROW_H);
+  }
+}
+
+/**
+ * @brief Rysuje wyśrodkowany tekst w określonym obszarze.
+ * @param text Tekst do wyświetlenia.
+ * @param x Współrzędna X lewego górnego rogu obszaru.
+ * @param y Współrzędna Y lewego górnego rogu obszaru.
+ * @param w Szerokość obszaru.
+ * @param h Wysokość obszaru.
+ */
+void drawCenteredText(const char* text, int x, int y, int w, int h) {
+  if (tftPtr) {
+    tftPtr->setTextColor(ST77XX_WHITE);
+    tftPtr->setTextSize(1);
+    int16_t x1, y1;
+    uint16_t w1, h1;
+    tftPtr->getTextBounds(text, 0, 0, &x1, &y1, &w1, &h1);
+    int tx = x + (w - w1) / 2 - x1;
+    int ty = y + (h - h1) / 2 - y1;
+    tftPtr->setCursor(tx, ty);
+    tftPtr->print(text);
+  }
+}
+
+/**
+ * @brief Rysuje wyśrodkowany tekst na całym ekranie (do komunikatów statusowych).
+ * @param text Tekst do wyświetlenia.
+ * @param color Kolor tekstu.
+ */
+void showCenteredStatusText(const String text, uint16_t color) {
+  if (tftPtr) {
+    tftPtr->fillScreen(ST77XX_BLACK);
+    tftPtr->setTextWrap(false);
+    tftPtr->setTextSize(1); // Większy rozmiar tekstu dla statusu
+    tftPtr->setTextColor(color);
+
+    int16_t x1, y1;
+    uint16_t w, h;
+    // TFT_WIDTH i TFT_HEIGHT nie są globalne w display.h, więc musimy je przekazać lub założyć
+    // Tutaj zakładamy, że SCREEN_WIDTH i SCREEN_HEIGHT są zdefiniowane w main.ino
+    // i używamy ich jako domyślnych rozmiarów ekranu
+    int screen_width_local = 320; // Możesz dostosować, jeśli znasz dokładny rozmiar ekranu
+    int screen_height_local = 170; // Możesz dostosować, jeśli znasz dokładny rozmiar ekranu
+
+    tftPtr->getTextBounds(text, 0, 0, &x1, &y1, &w, &h);
+
+    int16_t x = (screen_width_local - w) / 2 - x1;
+    int16_t y = (screen_height_local - h) / 2 - y1;
+
+    tftPtr->setCursor(x, y);
+    tftPtr->print(text);
+  }
+}
+
+
+/**
+ * @brief Rysuje sekcję zużycia paliwa.
+ * @param value Wartość zużycia paliwa.
+ * @param x Współrzędna X lewego górnego rogu sekcji.
+ * @param y Współrzędna Y lewego górnego rogu sekcji.
+ * @param w Szerokość sekcji.
+ * @param h Wysokość sekcji.
+ */
+void drawFuelConsumption(float value, int x, int y, int w, int h) {
+  if (tftPtr) {
+    tftPtr->setTextColor(ST77XX_GREEN);
+    tftPtr->fillRect(x, y, w, h, ST77XX_BLACK); // Wypełnienie tła czernią
+
+    tftPtr->setTextSize(2);
+    String valStr = String(value, 1); // Wartość z jednym miejscem po przecinku
+    int startX = x + 10;
+    int startY = y + (h / 2) + 12; // Pozycja Y dla tekstu
+
+    tftPtr->setCursor(startX, startY);
+    tftPtr->print(valStr);
+
+    int16_t bx, by;
+    uint16_t bw, bh;
+    tftPtr->getTextBounds(valStr, startX, startY, &bx, &by, &bw, &bh); // Pobranie wymiarów tekstu
+
+    tftPtr->setTextSize(1);
+    int unitX = bx + bw + 5; // Pozycja X dla jednostki
+    int unitY = startY - (bh / 2); // Pozycja Y dla jednostki
+
+    tftPtr->setCursor(unitX, unitY);
+    tftPtr->setTextColor(ST77XX_WHITE);
+    tftPtr->print("L/100km");
+  }
+}
+
+/**
+ * @brief Rysuje sekcję temperatury oleju.
+ * @param value Wartość temperatury oleju.
+ * @param x Współrzędna X lewego górnego rogu sekcji.
+ * @param y Współrzędna Y lewego górnego rogu sekcji.
+ * @param w Szerokość sekcji.
+ * @param h Wysokość sekcji.
+ */
+void drawOilTemp(float value, int x, int y, int w, int h) {
+  if (tftPtr) {
+    tftPtr->fillRect(x, y, w, h, ST77XX_BLACK);
+    tftPtr->setTextColor(ST77XX_YELLOW);
+    drawCenteredText("temp oleju", x, y, w, h / 2); // Rysowanie nagłówka
+    char buf[10];
+    snprintf(buf, sizeof(buf), "%.1f °C", value); // Formatowanie wartości
+    tftPtr->setCursor(x + (w - strlen(buf) * 6) / 2, y + h * 2 / 3); // Wyśrodkowanie wartości
+    tftPtr->print(buf);
+  }
+}
+
+/**
+ * @brief Rysuje sekcję temperatury płynu chłodniczego.
+ * @param value Wartość temperatury płynu chłodniczego.
+ * @param x Współrzędna X lewego górnego rogu sekcji.
+ * @param y Współrzędna Y lewego górnego rogu sekcji.
+ * @param w Szerokość sekcji.
+ * @param h Wysokość sekcji.
+ */
+void drawCoolantTemp(float value, int x, int y, int w, int h) {
+  if (tftPtr) {
+    tftPtr->fillRect(x, y, w, h, ST77XX_BLACK);
+    tftPtr->setTextColor(ST77XX_CYAN);
+    drawCenteredText("temp coolant", x, y, w, h / 2);
+    char buf[10];
+    snprintf(buf, sizeof(buf), "%.1f °C", value);
+    tftPtr->setCursor(x + (w - strlen(buf) * 6) / 2, y + h * 2 / 3);
+    tftPtr->print(buf);
+  }
+}
+
+/**
+ * @brief Rysuje sekcję obrotów na minutę (RPM).
+ * @param value Wartość RPM.
+ * @param x Współrzędna X lewego górnego rogu sekcji.
+ * @param y Współrzędna Y lewego górnego rogu sekcji.
+ * @param w Szerokość sekcji.
+ * @param h Wysokość sekcji.
+ */
+void drawRPM(int value, int x, int y, int w, int h) {
+  if (tftPtr) {
+    tftPtr->fillRect(x, y, w, h, ST77XX_BLACK);
+    tftPtr->setTextColor(ST77XX_MAGENTA);
+    drawCenteredText("RPM", x, y, w, h / 2);
+    char buf[10];
+    snprintf(buf, sizeof(buf), "%d", value);
+    tftPtr->setCursor(x + (w - strlen(buf) * 6) / 2, y + h * 2 / 3);
+    tftPtr->print(buf);
+  }
+}
+
+/**
+ * @brief Rysuje sekcję prędkości.
+ * @param value Wartość prędkości.
+ * @param x Współrzędna X lewego górnego rogu sekcji.
+ * @param y Współrzędna Y lewego górnego rogu sekcji.
+ * @param w Szerokość sekcji.
+ * @param h Wysokość sekcji.
+ */
+void drawSpeed(int value, int x, int y, int w, int h) {
+  if (tftPtr) {
+    tftPtr->fillRect(x, y, w, h, ST77XX_BLACK);
+    tftPtr->setTextColor(ST77XX_ORANGE);
+    drawCenteredText("SPEED", x, y, w, h / 2);
+    char buf[10];
+    snprintf(buf, sizeof(buf), "%d km/h", value);
+    tftPtr->setCursor(x + (w - strlen(buf) * 6) / 2, y + h * 2 / 3);
+    tftPtr->print(buf);
+  }
+}
+
+/**
+ * @brief Rysuje ogólny układ ekranu.
+ * @param SCREEN_WIDTH Całkowita szerokość ekranu.
+ * @param SCREEN_HEIGHT Całkowita wysokość ekranu.
+ * @param TOP_HEIGHT Wysokość górnej sekcji.
+ * @param topLeftW Szerokość lewej górnej sekcji.
+ * @param topLeftH Wysokość lewej górnej sekcji.
+ * @param topRightW Szerokość prawej górnej sekcji.
+ * @param topRightH Wysokość prawej górnej sekcji.
+ */
+void drawLayout(int SCREEN_WIDTH, int SCREEN_HEIGHT, int TOP_HEIGHT, int topLeftW, int topLeftH, int topRightW, int topRightH) {
+  if (tftPtr) {
+    tftPtr->fillScreen(ST77XX_BLACK); // Wyczyść ekran
+
+    // Górna lewa sekcja "Spalanie"
+    tftPtr->fillRect(0, 0, topLeftW, topLeftH, ST77XX_BLACK);
+
+    // Górna prawa sekcja na ikonę
+    tftPtr->fillRect(topLeftW, 0, topRightW, topRightH, ST77XX_BLACK);
+    drawExampleBitmap(topLeftW + (topRightW - ARROW_W) / 2, (topRightH - ARROW_H) / 2);
+  }
+}
+
+#endif
